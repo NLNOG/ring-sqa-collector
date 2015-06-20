0f2b0da4e63827a4b2bf6b7e84508a6b65fae920
Lochii Connectivity <lcreg-github@convergence.cx>
2015-06-20 10:44:34 +0000

    first shot at correlator
    Change defaults
    
    cluster significance score
    
    cluster pre-flight checks
    
    cluster work
    
    decouple db config
    
    more work on algorithm
    
    thus far
    
    add mwb model
    
    mwb to orm
    
    fix constraints
    
    objects
    
    change to cascade
    
    new mwb
    
    new mwb
    
    normalise key space
    
    ai
    
    new mwb
    
    new mwb
    
    stage one mfar
    
    Fix typo
    
    Fix module reference
    
    new mwb
    
    stage two mfar
    
    added event id to FE
    
    added event id to FE
    
    first draft UI
    
    NLNog -> NLNOG
    
    Fix steenbergen compliance problem
    
    Added additional debugging and better error handling, also added some command line flags to the correlator tool
    
    consider impact of PEP 266 on global variables
    
    More ORM robustness
    
    Remove debugging prior to m/r with master

--- sqa_collector.py
+++ sqa_collector.py
@@ -4,7 +4,7 @@ import ConfigParser, datetime, json, os, socket
 from flask import Flask, request
 from sqlalchemy import create_engine, and_, desc
 from sqlalchemy.orm import sessionmaker
-from sqa_collector_db import DECLARATIVE_BASE, SqaCollector
+from sqa_collector_db import DECLARATIVE_BASE, SqaCollector, SqaCorrelator, SqaCollectorCorrelator, SqaCorrelatorObject
 
 app = Flask(__name__, static_url_path='')
 app.debug = False
@@ -13,25 +13,37 @@ config = ConfigParser.ConfigParser()
 config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sqa_collector.conf')
 config.read(config_file)
 
+# Database connector string
+try:
+    db_conn_str = config.get('database', 'connection_string')
+except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
+    db_conn_str = 'mysql://sqa_collector:sqa_collector@localhost/sqa_collector'
+
+# Database pool recycle
+try:
+    db_pool_recycle = config.get('database', 'pool_recycle')
+except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
+    db_pool_recycle = 3600
+
 # Max results from database
 try:
-    max_results = config.get('output', 'max_results')    
-except (NoOptionError, NoSectionError):
+    max_results = config.get('output', 'max_results')
+except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
     max_results = 100
 
 # Default pagination size
 try:
-    per_page = config.get('output', 'per_page')    
+    per_page = config.get('output', 'per_page')
 except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
     per_page = 50
 
-
-enginestr = 'mysql://sqa_collector:sqa_collector@localhost/sqa_collector'
-engine = create_engine(enginestr)
+# Connect to DB and session
+engine = create_engine(db_conn_str, pool_recycle=db_pool_recycle)
 DECLARATIVE_BASE.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Routing
 @app.route('/jquery.dynatable.css', methods=['GET'])
 def dynatableCSS():
     return app.send_static_file('jquery.dynatable.css')
@@ -50,7 +62,7 @@ def display():
     <meta charset="utf-8">
     <title>SQA Collector</title>
     <meta name="description" content="SQA Collector">
-    <meta name="author" content="NLNog RING">
+    <meta name="author" content="NLNOG RING">
 
     <link href="/jquery.dynatable.css" rel="stylesheet">
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
@@ -63,7 +75,7 @@ def display():
 <body>
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
-        <a class="navbar-brand navbar-right" href="#">NLNog RING</a>
+        <a class="navbar-brand navbar-right" href="#">NLNOG RING</a>
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-col-md-4="navbar">
             <span class="sr-only">Toggle navigation</span>
@@ -83,13 +95,22 @@ def display():
     <div class="container">
       <div class="jumbotron">
         <h1>SQA Collector lookup service</hq>
-        <p class="lead">Welcome to the SQA Collector lookup service, below are the lastest SQA alerts. Times all in UTC.</font></p>
+        <p class="lead">Welcome to the SQA Collector lookup service, below are the latest SQA alerts. Times all in UTC.</font></p>
       </div>
       <div class="row">
 '''
-    html += '<table id="results" class="table table-bordered"><thead><tr><th>id</th><th>timestamp</th><th>raised_by</th><th>short</th></tr></thead><tbody>'
-    for alarm in session.query(SqaCollector).order_by(desc(SqaCollector.started)).limit(max_results):
-        html += "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" % (alarm.id, alarm.started, alarm.raised_by, alarm.short)
+    html += '<table id="results" class="table table-bordered"><thead><tr><th>major_event</th><th>alarm</th><th>timestamp</th><th>raised_by</th><th>short</th></tr></thead><tbody>'
+    for alarm in session.query(SqaCollector, SqaCollectorCorrelator).outerjoin(SqaCollectorCorrelator).order_by(desc(SqaCollector.started)).limit(max_results):
+        if alarm.SqaCollectorCorrelator:
+            event_id = alarm.SqaCollectorCorrelator.correlator_id if alarm.SqaCollectorCorrelator.correlator_id else 'None'
+        else:
+            event_id = 'None'
+        alarm_id = alarm.SqaCollector.id if alarm.SqaCollector.id else 'None'
+        started  = alarm.SqaCollector.started if alarm.SqaCollector.started else 'Unknown'
+        raisedby = alarm.SqaCollector.raised_by if alarm.SqaCollector.raised_by else 'Unknown'
+        short    = alarm.SqaCollector.short if alarm.SqaCollector.short else 'No description'
+
+        html += "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" % (event_id, alarm_id, started, raisedby, short)
     html += '</tbody></table>'
     html += '''
         </div>
@@ -99,35 +120,55 @@ def display():
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src="/jquery.dynatable.js"></script>
     <script>
-            $(document).ready(function() { 
+            $(document).ready(function() {
 '''
     html += '$.dynatableSetup({dataset: { perPageDefault: ' + str(per_page) + ' }, writers: { _rowWriter: tableRowWriter}});'
     html += '''
-                $('#results').dynatable(); 
+                $('#results').dynatable();
                 $('#results').css('cursor','pointer');
             });
 
             function tableRowWriter(rowIndex, record, columns, cellWriter) {
-                row = '<tr>' + tableRowTdMaker(record.id, record.id) + tableRowTdMaker(record.id, record.timestamp) + tableRowTdMaker(record.id, record.raised_by) + tableRowTdMaker(record.id, record.short) + '</tr>';
+                row = '<tr>' + tableRowEventTdMaker(record.major_event, record.major_event) + tableRowAlarmTdMaker(record.alarm, record.alarm) + tableRowAlarmTdMaker(record.alarm, record.timestamp) + tableRowAlarmTdMaker(record.alarm, record.raised_by) + tableRowAlarmTdMaker(record.alarm, record.short) + '</tr>';
                 return row;
             }
 
-            function tableRowTdMaker(id, body) {
-                row = '<td style="text-align: left;" onClick="showText(' + id + ');">' + body + '</td>';
+            function tableRowAlarmTdMaker(id, body) {
+                row = '<td style="text-align: left;" onClick="showAlarmText(' + id + ');">' + body + '</td>';
                 return row;
             }
 
-            function showText(id) {
-                var url = '/text/' + id;
-                $('#textModal').html('<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title">View Text - Alert ' + id + '</h4><a href="/view/' + id + '">permalink</a></div><div class="modal-body"><pre id="textModal-body-text"></pre><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
+            function tableRowEventTdMaker(id, body) {
+                if (id == "None") {
+                    row = '<td style="text-align: left;">' + body + '</td>';
+                }
+                else {
+                    row = '<td style="text-align: left;" onClick="showEventText(' + id + ');">' + body + '</td>';
+                }
+                return row;
+            }
+
+            function showAlarmText(id) {
+                var url = '/alarm_text/' + id;
+                $('#textModal').html('<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title">View Text - Alarm ' + id + '</h4><a href="/view_alarm/' + id + '">permalink</a></div><div class="modal-body"><pre id="textModal-body-text"></pre><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
                 $.ajax({url: url, success: function(result){
                     $('#textModal-body-text').text(result);
-                    $('#textModal').modal('show'); 
+                    $('#textModal').modal('show');
                     $('#textModal').css({ 'display': 'block' });
                 }});
             }
 
-    </script> 
+            function showEventText(id) {
+                var url = '/event_text/' + id;
+                $('#textModal').html('<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title">View Text - Event ' + id + '</h4><a href="/view_event/' + id + '">permalink</a></div><div class="modal-body"><pre id="textModal-body-text"></pre><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
+                $.ajax({url: url, success: function(result){
+                    $('#textModal-body-text').text(result);
+                    $('#textModal').modal('show');
+                    $('#textModal').css({ 'display': 'block' });
+                }});
+            }
+
+    </script>
 </body>
 </html>
 '''
@@ -148,7 +189,7 @@ def store():
                 print "Unrecognised sending host %s" % raised_by
                 return_code = 'FAIL 1'
         except Exception, e:
-            print e
+            app.logger.exception(e)
             return_code = 'FAIL 2'
 
         if blob['status'] == 'raise':
@@ -161,7 +202,7 @@ def store():
                     open_alarm.ended=datetime.datetime.today()
             session.commit()
     except Exception, e:
-        print e
+        app.logger.exception(e)
         return_code = 'FAIL 3'
     else:
         return_code = 'OK';
@@ -169,15 +210,15 @@ def store():
     session.close()
     return return_code
 
-@app.route('/view/<id>', methods=['GET'])
-def render_text(id):
+@app.route('/view_alarm/<id>', methods=['GET'])
+def render_alarm_text(id):
     html = '<pre>'
-    html += display_text(id)
+    html += display_alarm_text(id)
     html += '</pre>'
     return html
 
-@app.route('/text/<id>', methods=['GET'])
-def display_text(id):
+@app.route('/alarm_text/<id>', methods=['GET'])
+def display_alarm_text(id):
     html = 'No alarm text.'
     try:
         results = session.query(SqaCollector).filter(SqaCollector.id==id)
@@ -185,9 +226,55 @@ def display_text(id):
             for result in results:
                 html = result.long
     except Exception, e:
-        print e
+        app.logger.exception(e)
     session.close()
     return html
+
+@app.route('/view_event/<id>', methods=['GET'])
+def render_event_text(id):
+    html = '<pre>'
+    html += display_event_text(id)
+    html += '</pre>'
+    return html
+
+@app.route('/event_text/<id>', methods=['GET'])
+def display_event_text(id):
+    html = 'No event text.'
+    try:
+        results = session.query(SqaCorrelatorObject).filter(SqaCorrelatorObject.sqa_correlator_id==id)
+        if results:
+            if results.count() > 0:
+                html = 'The following were the top three contributors to the event:\n'
+                for result in results:
+                    html += "%s(%s%%) " % (result.object, result.percentage)
+            else:
+                html = 'No contributors could be determined for this event\n'
+    except Exception, e:
+        app.logger.exception(e)
+    session.close()
+    return html
+
+# Flask specifies that teardown functions should not raise.
+# However, they might not have their own error handling,
+# so we wrap them here to log any errors and prevent errors from
+# propagating.
+
+def wrap_teardown_func(teardown_func):
+    def log_teardown_error(*args, **kwargs):
+        try:
+            teardown_func(*args, **kwargs)
+        except Exception, e:
+            app.logger.exception(e)
+    return log_teardown_error
+
+if app.teardown_request_funcs:
+    for bp, func_list in app.teardown_request_funcs.items():
+        for i, func in enumerate(func_list):
+            app.teardown_request_funcs[bp][i] = wrap_teardown_func(func)
+
+if app.teardown_appcontext_funcs:
+    for i, func in enumerate(app.teardown_appcontext_funcs):
+        app.teardown_appcontext_funcs[i] = wrap_teardown_func(func)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
---
+++ sqa_correlator.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+import ConfigParser, dns.resolver, logging, os, re, socket, sys
+from collections import Counter
+from datetime import datetime, timedelta
+from dns import name, resolver, reversename
+from sets import Set
+from sqlalchemy import create_engine, and_, desc
+from sqlalchemy.orm import sessionmaker
+from sqa_collector_db import DECLARATIVE_BASE, SqaCollector, SqaCorrelator, SqaCollectorCorrelator, SqaCorrelatorObject
+
+config = ConfigParser.ConfigParser()
+config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sqa_collector.conf')
+config.read(config_file)
+
+## Config variables
+
+# Log file location
+try:
+    log_file = config.get('correlator', 'log_file')
+except (NoOptionError, NoSectionError):
+    log_file = '/var/log/sqa_correlator.log'
+
+# Database connector string
+try:
+    db_conn_str = config.get('database', 'connection_string')
+except (NoOptionError, NoSectionError):
+    db_conn_str = 'mysql://sqa_collector:sqa_collector@localhost/sqa_collector'
+
+# Hours to look back
+try:
+    seek_hours = config.get('correlator', 'seek_hours')
+except (NoOptionError, NoSectionError):
+    seek_hours   = 99999
+
+# Seconds to compare for delta
+try:
+    seek_seconds = config.get('correlator', 'seek_seconds')
+except (NoOptionError, NoSectionError):
+    seek_seconds = 30
+
+# Min number of events per cluster
+try:
+    seek_min = config.get('correlator', 'seek_min')
+except (NoOptionError, NoSectionError):
+    seek_min = 10
+
+# Min number of nodes down per cluster event
+try:
+    nodes_min = config.get('correlator', 'nodes_min')
+except (NoOptionError, NoSectionError):
+    nodes_min = 10
+
+# Traceroute min packet loss to be of concern
+try:
+    trace_lmin = config.get('correlator', 'trace_lmin')
+except (NoOptionError, NoSectionError):
+    trace_lmin = 10
+
+# List of whois servers to try
+try:
+    whois_servers = str(config.get('correlator', 'whois_servers')).split(',')
+except (NoOptionError, NoSectionError):
+    whois_servers = ['whois.ripe.net', 'whois.radb.net', 'whois.arin.net', 'whois.apnic.net', 'whois.lacnic.net', 'whois.afrinic.net']
+
+
+# Set up DB Engine and session
+engine = create_engine(db_conn_str)
+DECLARATIVE_BASE.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Start of code
+def main():
+
+    # Define seek_hours explicitly as a global, because it is 
+    # the only variable outside of main() we may manipulate
+    global seek_hours
+
+    # Start logging
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(process)d %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        filename=log_file)
+
+    # Bring logging to console if -d provided
+    if len(sys.argv) > 1:
+        if '-h' in sys.argv:
+            print 'Usage: ./sqa_correlator.py [-h|-d|-a]'
+            print ' -h = this help'
+            print ' -d = debugging to console'
+            print ' -a = perform correlation on all events, ignore seek_hours in config'
+            sys.exit(0)
+        if '-d' in sys.argv:
+            console = logging.StreamHandler()
+            console.setLevel(logging.DEBUG)
+            formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', '%Y-%m-%d %H:%M:%S')
+            console.setFormatter(formatter)
+            logging.getLogger('').setLevel(logging.DEBUG)
+            logging.getLogger('').addHandler(console)
+        if '-a' in sys.argv:
+            seek_hours = 999999
+
+    # Get the last $seek_hours of events
+    alarmbuf = {'cluster':{}}
+    clusterid = None
+    nextclusterid = 1
+    alarms = session.query(SqaCollector).filter(SqaCollector.started>=(datetime.today() - timedelta(hours = int(seek_hours))))
+    if alarms:
+        for alarm in alarms:
+            if alarmbuf.get('lastid'):
+                delta = alarm.started - alarmbuf['lasttime']
+                if delta.seconds <= int(seek_seconds):
+                    if clusterid is None:
+                        clusterid = nextclusterid
+                        nextclusterid+=1
+                    if alarmbuf['cluster'].get(clusterid) is None:
+                        alarmbuf['cluster'][clusterid] = Set()
+                    alarmbuf['cluster'][clusterid].add(alarmbuf['lastid'])
+                    alarmbuf['cluster'][clusterid].add(alarm.id)
+                else:
+                    clusterid = None
+                alarmbuf['lastdelta'] = delta
+            alarmbuf['lasttime'] = alarm.started
+            alarmbuf['lastid'] = alarm.id
+
+    # Scan interesting clusters, extract information about brokenness
+    for cluster in alarmbuf['cluster'].keys():
+        # Before we start looking into how big this cluster is, first cycle round it and 
+        # Find out if any alerms inside it are part of another , existing cluster in the db
+        # if so, just attach all of these alarms to the existing db cluster and bail out.
+        # We hope that the existing DB cluster is refined enough as a result of being at least
+        # $seek_min in size, with enough data to identify correlator objects
+        target_db_cluster = None
+        for sqa_id in alarmbuf['cluster'][cluster]:
+            existing_db_cluster = session.query(SqaCollectorCorrelator).filter(SqaCollectorCorrelator.collector_id == sqa_id)
+            if existing_db_cluster.count() > 0:
+                target_db_cluster = existing_db_cluster[0].correlator_id
+            elif target_db_cluster:
+                logging.debug("Adding sqa_id [%s] to cluster [%s]" % (sqa_id, target_db_cluster))
+                session.add(SqaCollectorCorrelator(collector_id=sqa_id, correlator_id=target_db_cluster))
+                session.commit()
+        if not target_db_cluster:
+            # So, if we didn't know about the cluster, carry on with our investigation.
+            # The first port of call is to address whether the cluster is $seek_min in size
+            if len(alarmbuf['cluster'][cluster]) >= int(seek_min):
+                adjacent_alarms = []
+                confirmed_alarms = []
+                problem_objects = []
+                for sqa_id in alarmbuf['cluster'][cluster]:
+                    # Get alarm
+                    alarm = session.query(SqaCollector).filter(SqaCollector.id == sqa_id)
+                    if alarm.count() == 1:
+                        alarm = alarm[0]
+                        # Look inside alarm for min number of nodes down
+                        nodes_down = re.search(r'(\d+) new nodes down', alarm.short)
+                        if nodes_down:
+                            if nodes_down.groups():
+                                if nodes_down.groups()[0] >= int(nodes_min):
+                                    #Note a confirmed alarm
+                                    confirmed_alarms.append(str(sqa_id))
+                                    #Start analysing the traceroutes
+                                    problem_nodes = []
+                                    if 'mtr' in alarm.long:
+                                        in_traceroute = None
+                                        last_problem_node = None
+                                        for line in alarm.long.splitlines():
+                                            if in_traceroute == 1:
+                                                line_parse = re.search(r'^\s+(\d+)\.\|--\s+(\S+)\s+(\S+)%\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)$', line)
+                                                if line_parse:
+                                                    # If you find a traceroute line with more than min packet loss, stash it
+                                                    # Next time we come around, if next line is non-zero also, then store it
+                                                    # We believe this makes the algorithm steenbergen compliant.
+                                                    if last_problem_node and float(line_parse.groups()[2]) > 0:
+                                                        logging.debug("In cluster %s, on alarm %s and Found compliant problem node %s from last problem node %s, with pl %s" % (cluster, sqa_id, line_parse.groups()[1], last_problem_node, line_parse.groups()[2]))
+                                                        problem_nodes.append(last_problem_node)
+                                                        last_problem_node = None
+                                                    if int(line_parse.groups()[0]) > 1 and float(line_parse.groups()[2]) > int(trace_lmin):
+                                                        logging.debug("In cluster %s, on alarm %s and Found potential problem node %s, with pl %s" % (cluster, sqa_id, line_parse.groups()[1], line_parse.groups()[2]))
+                                                        last_problem_node = line_parse.groups()[1]
+                                                else:
+                                                    in_traceroute = None
+                                                    last_problem_node = None
+                                            if 'mtr' in line or 'StDev' in line:
+                                                in_traceroute = 1
+                                    if len(problem_nodes) > 1:
+                                        # Find first problem node (which also isn't the only problem node), look up ASN and add to problem_objects for cluster
+                                        problem_node = problem_nodes[0]
+                                        problem_asn = find_asn(problem_node)
+                                        logging.debug("Translating problem_node [%s] into asn [%s]" % (problem_node, problem_asn))
+                                        problem_objects.append(problem_asn)
+                                    else:
+                                        logging.debug("No compliant problem nodes were found")
+                # Now summarise cluster issues and add them to DB
+                if len(confirmed_alarms) > 0:
+                    pcnt_problem_objects = pcnt_breakdown(problem_objects)
+                    sqa_correlator = SqaCorrelator()
+                    session.add(sqa_correlator)
+                    session.commit()
+                    logging.info("New Cluster lid [%s], Alarms [%s], ASN list [%s] added to db with dbid [%s]" % (cluster, ','.join(confirmed_alarms), pcnt_problem_objects, sqa_correlator.id))
+                    for pcnt_problem_object in pcnt_problem_objects:
+                        object_name, object_pcnt = pcnt_problem_object
+                        session.add(SqaCorrelatorObject(sqa_correlator_id=sqa_correlator.id, object=object_name, percentage=object_pcnt))
+                    for sqa_id in alarmbuf['cluster'][cluster]:
+                        logging.debug("Attaching sqa_id [%s] to cluster [%s] in db" % (sqa_id, sqa_correlator.id))
+                        session.add(SqaCollectorCorrelator(collector_id=sqa_id, correlator_id=sqa_correlator.id))
+                        
+           
+    session.commit()
+    session.close()
+
+# Look up ADN for an IP address using multiple means
+def find_asn(ip):
+    asn = find_asn_cymru(ip)
+    if asn:
+        return 'asn:' + asn
+    else:
+        netname = find_asn_whois(ip)
+        if netname:
+            return 'netname:' + netname
+        else:
+            return 'unknown'
+
+# Look up ASN for an IP address against Cymru, only works if the IP space is routed
+# Advantages = fast, cacheable
+def find_asn_cymru(ip):
+    logging.debug("called find_asn_cymru(%s)" % ip)
+    reverse_zone = 'ip6.arpa' if ':' in ip else 'in-addr.arpa'
+    origin_zone  = 'origin6.asn.cymru.com' if ':' in ip else 'origin.asn.cymru.com'
+    try:
+        # Convert address into format that can be sent to cymru lookup service
+        lookup_target = name.from_unicode(((str(reversename.from_address(ip)).replace(reverse_zone, origin_zone)).decode("utf-8")))
+        # Run query to cymru
+        lookup_result = resolver.query(lookup_target, 'TXT')
+        # Add result if found
+        if lookup_result:
+            asn = str(lookup_result[0]).replace('"','').split(' | ')[0]
+            return asn
+    except dns.resolver.NXDOMAIN:
+        return None
+    except:
+        # Generic exception handler, should look at logging exceptions in the future TODO
+        logging.error("some kind of exception in find_asn_cymru", exc_info=True)
+        return None
+
+# Look up ASN for an IP address against RADB instead
+def find_asn_whois(ip):
+    logging.debug("called find_asn_whois(%s)" % ip)
+    for whois_server in whois_servers:
+        msg = whois(whois_server, ip)
+        if msg:
+            m = re.search(r'(?i)netname:\s+(\S+)', msg)
+            if m:
+                # The RIPE, APNIC and LACNIC Responses for IPv6 space when it doesn't know and doesn't issue a referral
+                if m.groups()[0] == 'IANA-BLK' or m.groups()[0] == 'ROOT':
+                    continue
+                return m.groups()[0]
+    return None
+
+def whois(server, ip):
+    logging.debug("called whois(%s, %s)" % (server, ip))
+    try:
+        s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+        s.connect((server , 43))
+        if server == 'whois.arin.net':
+            s.send('n + ')
+        s.send(ip + '\r\n')
+        msg = ''
+        while len(msg) < 10000:
+            chunk = s.recv(100)
+            if(chunk == ''):
+                break
+            msg = msg + chunk
+        return msg
+    except:
+        return None
+
+# Get percentage breakdown for contributing ASN
+def pcnt_breakdown(asns):
+    result = []
+    counter = Counter(asn for asn in asns).most_common(3)
+    for countee in counter:
+        (asn, count) = countee
+        pcnt = int(100 * float(count) / float(len(asns)))
+        result.append([asn, pcnt])
+    return result
+
+if __name__ == '__main__':
+    main()
